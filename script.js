48467450701799b39f8fb0510e7d0fac462eb6df
Rahul Kumar <spartan024680@gmai.com>
2025-10-23 00:40:48 +0530

    Page1 done

--- script.js
+++ script.js
@@ -32,28 +32,51 @@ tl.from("#line1-part1",{
         h5timer.innerHTML = grow;
     }
 
-   },33);
+   },25);
 },
 });
 
 tl.to(".line h2",{
     animationName: "anime",
     opacity: 1
-})
+});
 
 tl.to("#loader",{
     opacity: 0,
     duration: 0.2,
-    delay: 4,
+    delay: 2.5,
     display:"none"
-})
+});
 
 tl.from("#page1",{
     y: 1600,
     opacity: 0,
     delay: 0.2,
     ease:Power4,
-    duration: 0.5
+    duration: 0.6
+});
+
+tl.from("#nav",{
+    opacity: 0
 })
+
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y: 120,
+    stagger: 0.2
+})
+
 }
 loadingAnimation();   
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left: dets.x,
+        top: dets.y
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
